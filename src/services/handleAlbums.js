643d31cee7ca639857c3ce01a929d0fe197241d8
Brenda Daroz <brenda.daroz@gmail.com>
2023-04-20 12:46:56 +0200

    add put function to handle changes in json

--- src/services/handleAlbums.js
+++ src/services/handleAlbums.js
@@ -4,4 +4,22 @@ const handleAlbums = async () => {
   return data
 }
 
-export default handleAlbums
+
+
+const putAlbum = async (album) => {
+  const response = await fetch(`http://localhost:3000/albums/${album.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(album)
+  })
+  console.log(response)
+  const data = await response.json()
+  return data
+}
+
+// patchAlbum()
+
+
+export {handleAlbums, putAlbum}
