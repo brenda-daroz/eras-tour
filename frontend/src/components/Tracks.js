9205c68011708eff35c75c73d43ac1f4c09ec31e
Brenda Daroz <brenda.daroz@gmail.com>
2023-06-23 18:57:16 +0200

    add tabs components to render different categories

--- frontend/src/components/Tracks.js
+++ frontend/src/components/Tracks.js
@@ -1,15 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import Track from "./Track";
+import Tabs from "./Tabs";
 
 
 const Wrapper = styled.div`{
-
   background: linear-gradient(to bottom, ${props => props.bgColor} 50%, transparent 100%);
   width: 20%;
   overflow: hidden;
   height: 100vh;
-
   @media only screen and (max-width: 700px) {
     width: 100%;
   }
@@ -24,7 +23,7 @@ const Ul = styled.ul`{
   width: fit-content;
   padding: 0 10px;
   margin: 30px auto;
-  height: 60%;
+  height: 45%;
   justify-content: center
 }`
 
@@ -32,10 +31,11 @@ const Li = styled.li`{
   list-style: none;
   text-transform: uppercase;
   font-weight: 500;
-  line-height: 1.35;
+  line-height: 1.7;
   color: ${props => props.fontColor};
   font-size: 0.85rem;
   text-align: center;
+  text-decoration: dotted;
   @media only screen and (max-width: 700px) {
     font-size: 1rem;
   }
@@ -50,25 +50,86 @@ const Img = styled.img`{
   display: block;
   height: 320px;
   overflow: hidden;
-  // z-index: -1;
-  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1.0) 40%, transparent 100%);
+  position: relative;
+  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.9) 70%, transparent 100%);
+  @media only screen and (max-width: 700px) {
+    zoom: 1.6;
 }`
 
-export default function Tracks({ tracks, color, image, title, latest }) {
+const Credit = styled.figcaption`{
+    // background-color: #000;
+    opacity: 0.8;
+    border-radius: 5px;
+    color: grey;
+    font-size: 0.7rem;
+    text-align: center;
+    padding: 3px 3px;
+    margin-top: -20px;
+    @media only screen and (max-width: 700px) {
+      padding: 0;
+      margin-top: -160px;
+      opacity: 1;
+      z-index: 10;
+      color: black;
+    }
+}`
+
+export default function Tracks({ tracks, color, image, title, latest, credit }) {
+
   return (
     <Wrapper bgColor={color.background}>
-      <Ul>
-        {tracks.map((track, i) => {
-          return (
-            <Li key={i} fontColor={color.default}>
-              <Track track={track} color={color} latest={latest} />
 
-            </Li>
-          )
-        }
-        )}
-      </Ul>
-      <Img src={image} alt={title}></Img>
+      <Tabs children={[
+        {
+          name: "Surprise", content:
+            <Ul>
+              {tracks.map((track, i) => {
+                if (track.status.type === "surprise") {
+                  return (
+                    TracksList(i, track)
+                  )
+                }
+              }
+              )}
+            </Ul>
+        },
+        {
+          name: "Fixed", content:
+            <Ul>
+              {tracks.map((track, i) => {
+                if (track.status.type === "fixed") {
+                  return (
+                    TracksList(i, track)
+                  )
+                }
+              }
+              )}
+            </Ul>
+        },
+        {
+          name: "Unplayed", content:
+            <Ul>
+              {tracks.map((track, i) => {
+                if (track.status.type === "unplayed") {
+                  return (
+                    TracksList(i, track)
+                  )
+                }
+              }
+              )}
+            </Ul>
+        },
+      ]} />
+      <figure>
+        <Img src={image} alt={title}></Img>
+        <Credit>{credit}</Credit>
+      </figure>
     </Wrapper>
   );
+
+  function TracksList(i, track) {
+    return <Li key={i} fontColor={color.default}>
+      <Track track={track} color={color} latest={latest} />
+    </Li>;
+  }
 }
