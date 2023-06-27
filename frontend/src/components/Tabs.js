fe5d53bc36e29fc47730cd811f180bfa200d61f4
Brenda Daroz <brenda.daroz@gmail.com>
2023-06-27 16:16:41 +0200

    fix color of tabs

--- frontend/src/components/Tabs.js
+++ frontend/src/components/Tabs.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 
 const Tab = styled.a`
-color : #EBEBEB;
+color : grey;
 cursor: pointer;
 opacity: 0.8;
 font-weight: 500;
@@ -19,10 +19,14 @@ font-size: 0.8rem;
   `}
 `;
 const ButtonGroup = styled.div`
-  margin: 10px 25px;
+  // margin: 10px 25px;
   display: flex;
   border: 0 5px 0 5px solid #ccc;
   justify-content: space-evenly;
+  padding: 8px 0;
+  background-color: #d3d3d3;
+  opacity: 0.6;
+  box-shadow: 0px 9px 24px -3px rgba(0,0,0,0.3);
 `;
 const types = ['Surprise', 'Fixed', 'Unplayed'];
 
