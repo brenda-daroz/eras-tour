b595810e6f29c09277489a9d7c3c88bd87ac0586
Brenda Daroz <brenda.daroz@gmail.com>
2023-07-12 18:17:51 +0200

    fixed position modal/prevent scroll

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const DarkBg = styled.div`{
   background-color: rgba(0, 0, 0, 0.2);
@@ -13,83 +12,117 @@ const DarkBg = styled.div`{
 }`
 
 const ModalDiv = styled.div`{
+  position: fixed;
   display: flex;
   align-items: center;
-  flex-direction: column;
   justify-content: space-evenly;
   gap: 10px;
+  height: 86%;
   background-color: white;
   border-radius: 5px;
-  width: 70%;
-  padding: 10px 20px;
+  width: 50%;
+  padding: 10px 10px;
   top: 50%;
   left: 50%;
   z-index: 11;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 300;
   transform: translate(-50%, -50%);
-  position: absolute;
+  // position: absolute;
   margin: 0 auto;
   @media only screen and (max-width: 700px) {
-    width: 50%;
+    width: 80%;
+    flex-direction: column;
   }
 }`
 
+const ModalInfo = styled.div`{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  height: 100%;
+  width: 50%;
+  padding: 10px 10px;
+  @media only screen and (max-width: 700px) {
+    gap: 5px;
+    height: fit-content;
+    width: 100%;
+  }
+}`
 const ModalText = styled.p`{
-  font-size: 0.7rem;
+  font-size: 0.9rem;
   text-align: center;
   padding: 0 5px;
   margin: 0;
 }`
 
 const ModalTtitle = styled.h3`{
-  font-size: 0.8rem;
+  font-size: 1rem;
   text-align: center;
   padding: 0 5px;
   margin: 0;
   text-transform: uppercase;
 }`
 
-const Anchor = styled.a`{
-  color: black;
-  text-decoration: none;
-  font-size: 0.8rem;
-  background-color: pink;
-  padding: 3px 6px;
-  border-radius: 5px;
-  margin-top: 5px;
-}`
 
 const CloseButton = styled.button`{
   position: absolute;
   top: 5px;
   right: 5px;
+  display: block;
   background-color: transparent;
   // border-radius: 50%;
   border: none;
   font-size: 1.2rem;
   color: #333;
   cursor: pointer;
-  // padding: 2px;
+  margin: 2px;
   &:hover {
     color: #EAAED0;
     transition: 0.3s ease-out;
   }
+  @media only screen and (max-width: 700px) {
+    right: 5px;
+
+  }
 }`
+
+const Video = styled.iframe`{
+  width: fit-content;
+  height: 100%;
+  border: none;
+  border-radius: 5px;
+  margin: 0 auto ;
+  @media only screen and (max-width: 700px) {
+    width: 100%;
+  }`
 
 
 const Modal = ({ setIsOpen, track }) => {
+  const regex = /video\/(\d+)/;
+  const match = track.video ? track.video.match(regex) : "";
+
+  const url = `https://www.tiktok.com/embed/v2/${match[1]}`;
   return (
     <>
       <DarkBg onClick={() => setIsOpen(false)}>
       </DarkBg>
       <ModalDiv onclick={(e) => e.stopPropagation()}>
         <CloseButton onClick={() => setIsOpen(false)}><i class="fa-solid fa-xmark"></i></CloseButton>
-        <ModalTtitle>{track.title}</ModalTtitle>
-        <ModalText>Date: {track.status.concertInfo.date}</ModalText>
-        <ModalText>Venue: {track.status.concertInfo.venue.name}</ModalText>
-        <ModalText>City: {track.status.concertInfo.venue.city.name}</ModalText>
-        <ModalText>Country: {track.status.concertInfo.venue.city.country.name}</ModalText>
-        {(track.video) ? <ModalText><Anchor href={track.video} target="_blank" rel="noreferrer">Live Performance (TikTok)</Anchor></ModalText> : null}
+        <ModalInfo>
+          <ModalTtitle>{track.title}</ModalTtitle>
+          <ModalText>Date: {track.status.concertInfo.date}</ModalText>
+          <ModalText>Venue: {track.status.concertInfo.venue.name}</ModalText>
+          <ModalText>Location: {track.status.concertInfo.venue.city.name} - {track.status.concertInfo.venue.city.country.name}</ModalText>
+        </ModalInfo>
+        <>
+          {(track.video) ? <Video src={url} frameborder="0" title="dasd"></Video> : null}
+        </>
+
       </ModalDiv>
+
     </>
   )
 }
