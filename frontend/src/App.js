5b061e801301538f03799d8ac6dc8db11058060c
Brenda Daroz <brenda.daroz@gmail.com>
2023-07-12 11:51:38 +0200

    insert loading component if no data yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,20 @@ import Tracks from './components/Tracks';
 import { handleData } from './services/handleAlbums';
 import { useEffect, useState } from "react";
 import Info from './components/Info';
+import Loader from './components/Loader';
 
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
+
+const Loading = styled.div`
+height: 100vh;
+width: 100vw;
+background-color: #f5f5f5;
+}`
+
+
 
 function App() {
   const [data, setData] = useState([])
@@ -29,20 +38,22 @@ function App() {
 
   return (
     <>
-    <Container>
-      {data.map((album, i) => {
-        return (
-          <Tracks
-            key={i}
-            image={album.cover}
-            credit={album.coverCredit}
-            color={album.color}
-            tracks={album.tracks}
-          />
-        )
-      })}
-    </Container>
-    <Info/>
+      <Container>
+
+        {data.length === 0 ? <Loading><Loader/></Loading> :
+          data.map((album, i) => {
+            return (
+              <Tracks
+                key={i}
+                image={album.cover}
+                credit={album.coverCredit}
+                color={album.color}
+                tracks={album.tracks}
+              />
+            )
+          })}
+      </Container>
+      <Info />
     </>
   );
 
