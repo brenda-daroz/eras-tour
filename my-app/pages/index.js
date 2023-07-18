700f0e88fe089b1b04e7818df7758d91eced97e6
Brenda Daroz <brenda.daroz@gmail.com>
2023-07-18 15:15:34 +0200

    add kofi widget, og tags and head

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,13 +1,50 @@
 import BigUseClient from "@/pages/components/BigUseClient";
 import { fazTudo } from "@/pages/api/setlist";
+import Head from "next/head";
 
 export default function Page({ data }) {
+  const kofi =
+  `<script>
+    kofiWidgetOverlay.draw('pixelbakery', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#ff38b8',
+    'floating-chat.donateButton.text-color': '#fff'
+    });
+  </script>`
 
   console.log("PAGEJS", data)
 
   return (
+
     <>
+      <Head>
+        <title>The Eras Tour</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Taylor Swift - The Eras Tour Surprise Songs Tracker" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://eras-tour.brenda.fyi" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Eras Tour" />
+        <meta property="og:description" content="The Eras Tour - Surprise songs tracker" />
+        <meta property="og:image"
+          content="https://res.cloudinary.com/dkminbfry/image/upload/v1688043299/Group_1_4_nzic5f.png" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="eras-tour.brenda.fyi" />
+        <meta property="twitter:url" content="https://eras-tour.brenda.fyi" />
+        <meta name="twitter:title" content="Eras Tour" />
+        <meta name="twitter:description" content="The Eras Tour - Surprise songs tracker" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/dkminbfry/image/upload/v1688041171/Group_2_ald1rg.png" />
+
+        {/* Kofi */}
+        <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js' />
+      </Head>
       <BigUseClient data={data} />
+ <div dangerouslySetInnerHTML={{ __html: kofi }}></div>
     </>
   );
 }
