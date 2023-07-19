19d3070064a9c7f7b338285645550801930cc0c2
Brenda Daroz <brenda.daroz@gmail.com>
2023-07-19 11:59:17 +0200

    configure digitalocean

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: '_static',
+  images: {
+    unoptimized: true
+  },
   compiler: {
     styledComponents: true,
   },
