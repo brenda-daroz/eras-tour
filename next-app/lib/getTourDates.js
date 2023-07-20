64f02365caf19973208aa0f5fde661ce9f70f9b4
Brenda Daroz <brenda.daroz@gmail.com>
2023-07-20 14:55:05 +0200

    scrape tour dates

---
+++ next-app/lib/getTourDates.js
@@ -0,0 +1,26 @@
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+
+const getTourDates = async () => {
+  // const res = await fetch("https://www.taylorswift.com/tour/")
+  const data = fs.readFileSync("foo.html", "utf8")
+  // console.log(data.toString())
+  // const html = await data.text()
+
+  const dom = new JSDOM(data)
+  const document = dom.window.document
+
+  const events = document.querySelectorAll('.event')
+  const tourDates = Array.from(events).map(event => {
+    const venue = event.querySelector('.event-venue').textContent.replace(/\s+/g, ' ').trim()
+    const city = event.querySelector('.event-city').textContent.replace(/\s+/g, ' ').trim()
+    const date = event.querySelector('.event-date')?.textContent.replace(/\s+/g, ' ').trim()
+    return { date, location: { venue, city } }
+  }
+  )
+
+  console.log('tourDates', tourDates)
+  return ""
+}
+
+export default getTourDates
