08bdf6807b84a61670cd2ca7c9bc3b76f6456ad7
Brenda Daroz <brenda.daroz@gmail.com>
2023-07-21 13:21:42 +0200

    update text info

--- next-app/pages/500.js
+++ next-app/pages/500.js
@@ -1,9 +1,9 @@
 export default function Custom500() {
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-      <p style={{ color: "purple", fontSize: "1.1rem" }}>Oooops, some error occurred.</p>
+      <p style={{ color: "purple", fontSize: "1.1rem" }}>Oooops, something went wrong.</p>
       <iframe src="https://giphy.com/embed/WQCv56djMTCmQkySAg" style={{ border: 0 }} width="378" height="480" class="giphy-embed" allowFullScreen></iframe>
-      <h1 style={{ color: "purple" }}>And refresh the page.</h1>
+      <h1 style={{ color: "purple" }}>Please, refresh the page.</h1>
     </div>
   )
 }
