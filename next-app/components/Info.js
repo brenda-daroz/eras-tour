87fef3265b90b413ecfdf1bdbe6db0106305349a
Brenda Daroz <brenda.daroz@gmail.com>
2023-07-26 17:42:55 +0200

    update info of author

--- next-app/components/Info.js
+++ next-app/components/Info.js
@@ -23,7 +23,7 @@ padding: 20px 200px;
 const Container = styled.footer`
   height: 8vh;
   display: flex;
-  justify-content: space-between;
+  justify-content:center;
   align-items: center;
   padding: 0 50px;
   @media only screen and (max-width: 700px) {
@@ -43,12 +43,13 @@ const Container = styled.footer`
 const Author = styled.div`
   font-size: 1rem;
   font-family: "Anonymous Pro", sans-serif;
+  text-align: center;
     a {
     text-decoration: none;
-    color: #020E69;
+    color: #EAAED0;
     font-weight: 700;
     &:hover {
-      color: #EAAED0;
+      color: #333;
       transition: 0.3s ease-out;
     }
     }
@@ -123,16 +124,17 @@ const Info = () => {
     <Wrapper>
       <Header>
         <Title>Taylor Swift - The Eras Tour Surprise Songs Tracker</Title>
-        <Text>Website built by a swiftie to all swifties.</Text>
+        <Author>Website built by a <a href="https://www.brenda.fyi" target="_blank" rel="noreferrer">swiftie </a>to all swifties.</Author>
+        {/* <Text>Website built by a swiftie to all swifties.</Text> */}
         <Text>
           With all Taylor's albums organized, you'll find her repertoire divided into three categories: Surprise, Fixed, and Unplayed. The Surprise section showcases the songs she's already performed, leaving you on the edge of your seat for what might come next. The Fixed category features the must-hear songs that grace every concert, ensuring you don't miss out on any fan-favorite classics. Lastly, the Unplayed section builds up anticipation as it lists the songs Taylor has yet to unveil on this tour.
         </Text>
         <Text>
-          By clicking on the surprise songs, a magical modal window appears, revealing details such as the date and location of the performance. But that's not all! To make your experience even more exciting, each surprise song comes with a special treat—a link to a TikTok fan video capturing the unforgettable moments from that particular performance. It's like having a front-row seat to Taylor's spectacular show!
+          By clicking on the surprise songs, a magical modal window appears, revealing details such as the date and location of the performance. But that's not all! To make your experience even more exciting, each surprise song comes with a special TikTok fan video capturing the unforgettable moments from that particular performance. It's like having a front-row seat to Taylor's concert!
         </Text>
       </Header>
       <Container>
-        <Author>Made by <a href="https://www.brenda.fyi" target="_blank" rel="noreferrer">brenda daroz</a> © 2023<span><FontAwesomeIcon icon={faHeart} style={{ color: "#9512af", }}/></span></Author>
+        {/* <Author>Made by <a href="https://www.brenda.fyi" target="_blank" rel="noreferrer">brenda daroz</a> © 2023<span><FontAwesomeIcon icon={faHeart} style={{ color: "#9512af", }}/></span></Author> */}
         <SocialMedia>
           <a href="https://www.linkedin.com/in/brendadz/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /></a>
           <a href="https://github.com/brenda-daroz" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}/></a>
