d03f0180ecca152b41468f7a189f49c486ba1463
Brenda Daroz <brenda.daroz@gmail.com>
2023-08-05 18:09:39 +0200

    add transition to tab

--- next-app/components/Tabs.js
+++ next-app/components/Tabs.js
@@ -3,19 +3,23 @@
 import styled from "styled-components";
 import { useState } from "react";
 
-const Tab = styled.a`
+const Tab = styled.li`
 color : grey;
+list-style: none;
 cursor: pointer;
 opacity: 0.8;
 font-weight: 500;
 padding : 5px 15px;
 font-size: 0.8rem;
+margin: 0 5px;
+border-bottom: 1px solid transparent;
   ${({ $active }) =>
     $active &&
     `
     border-bottom: 1px solid rgba(0,0,0, 0.5);
     opacity: 1;
     color: #333;
+    transition: 0.5s ease;
 
   `}
   @media only screen and (min-width: 1600px) {
@@ -25,8 +29,8 @@ font-size: 0.8rem;
     font-size: 1.1rem;
   }
 `;
-const ButtonGroup = styled.div`
-  // margin: 10px 25px;
+const ButtonGroup = styled.ul`
+  margin:0;
   display: flex;
   border: 0 5px 0 5px solid #ccc;
   justify-content: space-evenly;
@@ -35,6 +39,7 @@ const ButtonGroup = styled.div`
   opacity: 0.6;
   box-shadow: 0px 9px 24px -3px rgba(0,0,0,0.3);
 `;
+
 const types = ['Surprise', 'Fixed', 'Unplayed'];
 
 function Tabs({ children }) {
