3dfa854a1ee67b5b53836bc2ebedd46cc1fdf7a2
Brenda Daroz <brenda.daroz@gmail.com>
2023-11-10 15:37:57 +0100

    update formatting

--- next-app/components/Modal.js
+++ next-app/components/Modal.js
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faXmark} from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const DarkBg = styled.div`
   background-color: rgba(0, 0, 0, 0.2);
@@ -13,7 +13,7 @@ const DarkBg = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-`
+`;
 
 const ModalDiv = styled.div`
   position: fixed;
@@ -30,7 +30,7 @@ const ModalDiv = styled.div`
   top: 50%;
   left: 50%;
   z-index: 11;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   font-weight: 300;
   transform: translate(-50%, -50%);
   margin: 0 auto;
@@ -38,7 +38,7 @@ const ModalDiv = styled.div`
     width: 80%;
     flex-direction: column;
   }
-  @media (min-width:641px) and (max-width: 1024px) {
+  @media (min-width: 641px) and (max-width: 1024px) {
     width: 85%;
     height: 75%;
   }
@@ -46,7 +46,7 @@ const ModalDiv = styled.div`
     width: 40%;
     height: 55%;
   }
-`
+`;
 
 const ModalInfo = styled.div`
   display: flex;
@@ -57,11 +57,11 @@ const ModalInfo = styled.div`
   height: 100%;
   width: 50%;
   padding: 10px 0px;
-  @media only screen and (max-width:  641px) {
+  @media only screen and (max-width: 641px) {
     min-height: fit-content;
     width: 100%;
   }
-`
+`;
 const ModalText = styled.p`
   font-size: 0.9rem;
   text-align: center;
@@ -70,7 +70,7 @@ const ModalText = styled.p`
   @media only screen and (min-width: 2000px) {
     font-size: 1.2rem;
   }
-`
+`;
 
 const ModalTtitle = styled.h3`
   font-size: 1rem;
@@ -81,8 +81,7 @@ const ModalTtitle = styled.h3`
   @media only screen and (min-width: 2000px) {
     font-size: 1.4rem;
   }
-`
-
+`;
 
 const CloseButton = styled.button`
   position: absolute;
@@ -96,85 +95,102 @@ const CloseButton = styled.button`
   cursor: pointer;
   margin: 2px;
   &:hover {
-    color: #EAAED0;
+    color: #eaaed0;
     transition: 0.3s ease-out;
   }
   @media only screen and (max-width: 641px) {
     right: 5px;
-
   }
-`
+`;
 
 const Wrapper = styled.div`
   display: flex;
   gap: 20px;
   width: 100%;
   align-items: center;
-  @media only screen and (max-width:  641px) {
+  @media only screen and (max-width: 641px) {
     position: relative;
     max-height: inherit;
     flex-direction: column;
   }
-`
+`;
 
 const Video = styled.iframe`
   width: fit-content;
   height: 100%;
   border: none;
   border-radius: 5px;
-  margin: 0 auto ;
-  @media only screen and (max-width:  641px) {
-   height: inherit;
+  margin: 0 auto;
+  @media only screen and (max-width: 641px) {
+    height: inherit;
   }
-`
+`;
 
 const ModalCard = styled.div`
   background-color: #ebebeb;
   padding: 10px;
   width: 70%;
   border-radius: 10px;
-`
-
-
+`;
 
 const Modal = ({ setIsOpen, track }) => {
   const regex = /video\/(\d+)/;
   const match = track.video ? track.video.match(regex) : "";
   const url = `https://www.tiktok.com/embed/v2/${match[1]}`;
 
-
   return (
     <>
-      <DarkBg onClick={() => setIsOpen(false)}>
-      </DarkBg>
+      <DarkBg onClick={() => setIsOpen(false)}></DarkBg>
       <ModalDiv onclick={(e) => e.stopPropagation()}>
-      <Wrapper>
-        <CloseButton onClick={() => setIsOpen(false)}><FontAwesomeIcon icon={faXmark} /></CloseButton>
-        <ModalInfo>
-          <ModalTtitle>{track.title}</ModalTtitle>
+        <Wrapper>
+          <CloseButton onClick={() => setIsOpen(false)}>
+            <FontAwesomeIcon icon={faXmark} />
+          </CloseButton>
+          <ModalInfo>
+            <ModalTtitle>{track.title}</ModalTtitle>
 
-          {track.status.concertInfo.map((info, i) => {
-            return (
-
-              <ModalCard>
-              {track.status.concertInfo.length > 1 ? <p style={{margin: "0 0 4px 0"}} key={i}>Day {i+1}</p> : null}
-              <ModalText key={i}>Date: {info.date}</ModalText>
-              <ModalText key={i}>Venue: {info.venue.name}</ModalText>
-              <ModalText key={i}>Location: {info.venue.city.name} - {info.venue.city.country.code}</ModalText>
-              <ModalText>{track.status.instrument?.[0] === "piano" ? "🎹" : "🎸"}</ModalText>
-              </ModalCard>
-            )
-          }
-          )}
-        </ModalInfo>
-        <>
-          {(track.video) ? <div style={{height: '700px', position: 'sticky', top: '0', margin: '0 auto'}}> <Video src={url} frameBorder="0" title="dasd"></Video></div> : null}
-        </>
+            {track.status.concertInfo.map((info, i) => {
+              return (
+                <ModalCard>
+                  {track.status.concertInfo.length > 1 ? (
+                    <p style={{ margin: "0 0 4px 0" }} key={i}>
+                      Day {i + 1}
+                    </p>
+                  ) : null}
+                  <ModalText key={i}>Date: {info.date}</ModalText>
+                  <ModalText key={i}>Venue: {info.venue.name}</ModalText>
+                  <ModalText key={i}>
+                    Location: {info.venue.city.name} -{" "}
+                    {info.venue.city.country.code}
+                  </ModalText>
+                  <ModalText>
+                    {track.status.instrument?.[0] === "piano" ? "🎹" : "🎸"}
+                  </ModalText>
+                </ModalCard>
+              );
+            })}
+          </ModalInfo>
+          <>
+            {track.video ? (
+              <div
+                style={{
+                  height: "700px",
+                  position: "sticky",
+                  top: "0",
+                  margin: "0 auto",
+                }}
+              >
+                {" "}
+                <Video src={url} frameBorder="0" title="dasd"></Video>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </>
         </Wrapper>
       </ModalDiv>
-
     </>
-  )
-}
+  );
+};
 
 export default Modal;
