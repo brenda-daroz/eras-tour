0ca2b4f5dddb1ea4077ffb2a075b3f1ee4307d3c
Brenda Daroz <brenda.daroz@gmail.com>
2023-11-12 13:34:24 +0100

    lint format and remove comments

--- next-app/components/Tabs.js
+++ next-app/components/Tabs.js
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 
 import styled from "styled-components";
 import { useState } from "react";
 
 const Tab = styled.li`
-color : grey;
-list-style: none;
-cursor: pointer;
-opacity: 0.8;
-font-weight: 500;
-padding : 5px 15px;
-font-size: 0.8rem;
-margin: 0 5px;
-border-bottom: 1px solid transparent;
+  color: grey;
+  list-style: none;
+  cursor: pointer;
+  opacity: 0.8;
+  font-weight: 500;
+  padding: 5px 15px;
+  font-size: 0.8rem;
+  margin: 0 5px;
+  border-bottom: 1px solid transparent;
   ${({ $active }) =>
     $active &&
     `
@@ -25,22 +25,22 @@ border-bottom: 1px solid transparent;
   @media only screen and (min-width: 1600px) {
     font-size: 1.1rem;
   }
-  @media (min-width:768px) and (max-width: 1024px) {
+  @media (min-width: 768px) and (max-width: 1024px) {
     font-size: 1.1rem;
   }
 `;
 const ButtonGroup = styled.ul`
-  margin:0;
+  margin: 0;
   display: flex;
   border: 0 5px 0 5px solid #ccc;
   justify-content: space-evenly;
   padding: 5px 0px;
   background-color: #d3d3d3;
   opacity: 0.6;
-  box-shadow: 0px 9px 24px -3px rgba(0,0,0,0.3);
+  box-shadow: 0px 9px 24px -3px rgba(0, 0, 0, 0.3);
 `;
 
-const types = ['Surprise', 'Fixed', 'Unplayed'];
+const types = ["Surprise", "Fixed", "Unplayed"];
 
 function Tabs({ children }) {
   const [active, setActive] = useState(types[0]);
@@ -48,7 +48,7 @@ function Tabs({ children }) {
   return (
     <>
       <ButtonGroup>
-        {types.map(type => (
+        {types.map((type) => (
           <Tab
             key={type}
             name={type}
@@ -59,12 +59,11 @@ function Tabs({ children }) {
           </Tab>
         ))}
       </ButtonGroup>
-      {children.find(child => {
-        // if (child.name === active) return child.content;
-        return child.name === active
-        // return null;
+      {
+        children.find((child) => {
+          return child.name === active;
+        }).content
       }
-      ).content}
     </>
   );
 }
