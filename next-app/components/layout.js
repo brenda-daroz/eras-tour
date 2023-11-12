461c0d2816819343e4c3a5b2e3e1c0cbdc769ed7
Brenda Daroz <brenda.daroz@gmail.com>
2023-11-12 13:34:06 +0100

    lint format

--- next-app/components/layout.js
+++ next-app/components/layout.js
@@ -1,10 +1,7 @@
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
