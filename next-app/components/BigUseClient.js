2a408a6a530675e9812f702e77a0da312444b054
Brenda Daroz <brenda.daroz@gmail.com>
2024-02-11 12:23:24 +0100

    add logic filter by year

--- next-app/components/BigUseClient.js
+++ next-app/components/BigUseClient.js
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 
 import styled from "styled-components";
 import Tracks from "./Tracks";
 import Loader from "./Loader";
 import Info from "./Info";
+import { useState } from "react";
+import FabYear from "./fab";
 
 const Container = styled.div`
   display: flex;
@@ -11,18 +13,34 @@ const Container = styled.div`
 `;
 
 const Loading = styled.div`
-height: 100vh;
-width: 100vw;
-background-color: #f5f5f5;
-`
+  height: 100vh;
+  width: 100vw;
+  background-color: #f5f5f5;
+`;
 
 export default function BigUseClient({ data }) {
+  const [year, setYear] = useState(2024);
+  const dataYear =
+    year === 2024 ? data.data2024 : year === 2023 ? data.data2023 : data.data;
+
+  const options = [
+    { name: "All", func: () => setYear() },
+    { name: "2023", func: () => setYear(2023) },
+    { name: "2024", func: () => setYear(2024) },
+  ];
+
+  console.log(year);
+
   return (
     <>
       <Container>
-
-        {data.length === 0 ? <Loading><Loader /></Loading> :
-          data.map((album, i) => {
+        <FabYear options={options} year={options.name | year} />
+        {dataYear.length === 0 ? (
+          <Loading>
+            <Loader />
+          </Loading>
+        ) : (
+          dataYear.map((album, i) => {
             return (
               <Tracks
                 key={i}
@@ -31,9 +49,11 @@ export default function BigUseClient({ data }) {
                 color={album.color}
                 tracks={album.tracks}
                 title={album.title}
+                year={year}
               />
-            )
-          })}
+            );
+          })
+        )}
       </Container>
       <Info />
     </>
