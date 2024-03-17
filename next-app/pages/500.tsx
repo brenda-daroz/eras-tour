681c6c6c7e2189f7332a7cc9820cc86601b6a8e6
Brenda Daroz <brenda.daroz@gmail.com>
2024-03-17 20:27:32 +0100

    use ts

--- next-app/pages/500.tsx
+++ next-app/pages/500.tsx
@@ -1,8 +1,9 @@
+import React from "react"
 export default function Custom500() {
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
       <p style={{ color: "purple", fontSize: "1.1rem" }}>Oooops, something went wrong.</p>
-      <iframe src="https://giphy.com/embed/WQCv56djMTCmQkySAg" style={{ border: 0 }} width="378" height="480" class="giphy-embed" allowFullScreen></iframe>
+      <iframe src="https://giphy.com/embed/WQCv56djMTCmQkySAg" style={{ border: 0 }} width="378" height="480" className="giphy-embed" allowFullScreen></iframe>
       <h1 style={{ color: "purple" }}>Please, refresh the page.</h1>
     </div>
   )
