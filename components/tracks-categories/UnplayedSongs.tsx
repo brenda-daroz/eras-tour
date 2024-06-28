f3b5a0c5ab2f617c85fd3e6c9444037c22803253
Brenda Daroz <brenda.daroz@gmail.com>
2024-06-28 13:40:21 +0200

    add emoji to all played songs album

--- components/tracks-categories/UnplayedSongs.tsx
+++ components/tracks-categories/UnplayedSongs.tsx
@@ -1,18 +1,30 @@
 import React, { ComponentProps } from "react";
 import { Ul, TrackListItem } from "./SurpriseSongs";
 import { UITrack } from "@/lib/logic";
+import styled from "styled-components";
 
 type UnplayedTracksProps = {
   unplayedTracks: Array<UITrack>;
   color: ComponentProps<typeof TrackListItem>["color"];
 };
 
+const Message = styled.div`
+  font-size: 2.5rem;
+  text-align: center;
+`;
+
 export function UnplayedTracks({ unplayedTracks, color }: UnplayedTracksProps) {
   return (
-    <Ul>
-      {unplayedTracks.map((track, i) => {
-        return TrackListItem({ i, track, color });
-      })}
-    </Ul>
+    <div>
+      {unplayedTracks.length > 0 ? (
+        <Ul>
+          {unplayedTracks.map((track, i) => (
+            <TrackListItem key={i} i={i} track={track} color={color} />
+          ))}
+        </Ul>
+      ) : (
+        <Message>🎉</Message>
+      )}
+    </div>
   );
 }
