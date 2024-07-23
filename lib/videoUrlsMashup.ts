9c2aefd140e91a6e82629fdcdb677abbbc16eeed
Brenda Daroz <brenda.daroz@gmail.com>
2024-07-23 19:02:32 +0200

    rename file and add more videos

---
+++ lib/videoUrlsMashup.ts
@@ -0,0 +1,152 @@
+export const videoURLs: Record<string, string> = {
+  "Paper Rings / Stay Stay Stay_19-07-2024":
+    "https://www.tiktok.com/@mnttm/video/7394486863135821088?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "it's time to go / Better Man_19-07-2024":
+    "https://www.tiktok.com/@folletvalent/video/7393666797276351776?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Speak Now / Hey Stephen_18-07-2024":
+    "https://www.tiktok.com/@mimi_tx/video/7393579216333294880?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "this is me trying / Labyrinth_18-07-2024":
+    "https://www.tiktok.com/@callmejoena/video/7394425195051748641?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Superstar / invisible string_17-07-2024":
+    "https://www.tiktok.com/@noitsbeckx/video/7392771343999110432?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  '"Slut!" / False God_17-07-2024':
+    "https://www.tiktok.com/@alltoolynn/video/7392765447948471585?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Mr. Perfectly Fine / Red_14-07-2024":
+    "https://www.tiktok.com/@rickvevo/video/7392676099727068448?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Getaway Car / Out of the Woods_14-07-2024":
+    "https://www.tiktok.com/@margotxxo/video/7391879828984565024?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The 1 / Wonderland_13-07-2024":
+    "https://www.tiktok.com/@federicapangallo/video/7391922798198820129?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "I Almost Do / The Moment I Knew_13-07-2024":
+    "https://www.tiktok.com/@stateofkyra/video/7391294228384550177?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Mary's Song (Oh My My My) / So High School / Everything Has Changed_06-07-2024":
+    "https://www.tiktok.com/@cynthia.vanes/video/7388664822491122976?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Sweeter Than Fiction / Holy Ground_06-07-2024":
+    "https://www.tiktok.com/@sabri.hmn/video/7388632543848959264?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "You Are in Love / cowboy like me_05-07-2024":
+    "https://www.tiktok.com/@slaywhatyouwannaslaayy/video/7388279825384934689?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "imgonnagetyouback / Dress_05-07-2024":
+    "https://www.tiktok.com/@caithlinnetje/video/7388270647585049889?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Clara Bow / The Lucky One_30-06-2024":
+    "https://www.tiktok.com/@feverdreamhigh_/video/7386813594471632160?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Archer / Question...?_04-07-2024":
+    "https://www.tiktok.com/@littlefreakcherice/video/7388291269627252000?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Guilty as Sin? / Untouchable_04-07-2024":
+    "https://www.tiktok.com/@co.ncert/video/7387919880676199713?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Albatross / Dancing With Our Hands Tied_29-06-2024":
+    "https://www.tiktok.com/@tayfield_/video/7386144177488170272?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "This Love / Ours_29-06-2024":
+    "https://www.tiktok.com/@rachelkirk.x/video/7386065697056574752?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "State of Grace / You're on Your Own, Kid_28-06-2024":
+    "https://www.tiktok.com/@n0t_rusty/video/7385713105851993360?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Sweet Nothing / hoax_28-06-2024":
+    "https://www.tiktok.com/@brrussels/video/7385710551713729799?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Out of the Woods / Is It Over Now? / Clean_23-06-2024":
+    "https://www.tiktok.com/@swiftiesupremacy.13/video/7350254816506957061?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "thanK you aIMee / Mean_22-06-2024":
+    "https://www.tiktok.com/@dazzlinghazemerch/video/7383492037242309921?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Hits Different / Death by a Thousand Cuts_21-06-2024":
+    "https://www.tiktok.com/@ling2p/video/7383116682467740960?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Black Dog / Come Back… Be Here / Maroon_21-06-2024":
+    "https://www.tiktok.com/@ling2p/video/7383123907919858976?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "I Forgot That You Existed / This Is Why We Can't Have Nice Things_18-06-2024":
+    "https://www.tiktok.com/@swiftiesupremacy.13/video/7382169449996061958?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "I Hate It Here / the lakes_18-06-2024":
+    "https://www.tiktok.com/@megjon.es/video/7382341241469537569?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "no body, no crime / carolina_15-06-2024":
+    "https://www.tiktok.com/@abi.small/video/7380927096924704032?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Manuscript / Red_15-06-2024":
+    "https://www.tiktok.com/@tabsvalentin.e/video/7381101977683479841?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "This Is What You Came For / gold rush_14-06-2024":
+    "https://www.tiktok.com/@beccamarine217/video/7380486726931713311?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Great War / You're Losing Me_14-06-2024":
+    "https://www.tiktok.com/@laurawheatley/video/7380655062512602401?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "I Can See You / Mine_13-06-2024":
+    "https://www.tiktok.com/@imalicks/video/7380364994522746144?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Cornelia Street / Maroon_13-06-2024":
+    "https://www.tiktok.com/@chaojiewen/video/7381195003147619616?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "dorothea / It's Nice to Have a Friend_09-06-2024":
+    "https://www.tiktok.com/@elephondo/video/7378650519339797793?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Haunted / exile_09-06-2024":
+    "https://www.tiktok.com/@swiftnewyork/video/7378636422342823210?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Bolter / Getaway Car_08-06-2024":
+    "https://www.tiktok.com/@t.swift1312/video/7378246772705021217?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "All of the Girls You Loved Before / Crazier_08-06-2024":
+    "https://www.tiktok.com/@swiftiesupremacy.13/video/7378548323310980357?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Would've, Could've, Should've / I Know Places_07-06-2024":
+    "https://www.tiktok.com/@megan.clinch/video/7377889202605739297?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "'tis the damn season / Daylight_07-06-2024":
+    "https://www.tiktok.com/@ohheeyygurrlll/video/7377964878952189216?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Glitch / Everything Has Changed_03-06-2024":
+    "https://www.tiktok.com/@nanaisv/video/7376592826013912352?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Prophecy / long story short_02-06-2024":
+    "https://www.tiktok.com/@r_carli/video/7376025255933971745?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Fifteen / You're on Your Own, Kid_02-06-2024":
+    "https://www.tiktok.com/@laurielessard13/video/7376271484643298603?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Our Song / Jump Then Fall":
+    "https://www.tiktok.com/@gmcris7/video/7376182204856126753?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Sparks Fly / I Can Fix Him (No Really I Can)_29-05-2024":
+    "https://www.tiktok.com/@maysconcerts/video/7374533319226133793?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "I Look in People's Windows / Snow on the Beach_29-05-2024":
+    "https://www.tiktok.com/@_dearreader__/video/7374537390888668448?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Tortured Poets Department / Now That We Don't Talk_25-05-2024":
+    "https://www.tiktok.com/@kaarikiipus/video/7373091540098420000?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "You're on Your Own, Kid / Long Live_25-05-2024":
+    "https://www.tiktok.com/@invisblestrings/video/7373060486545378592?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Come Back…Be Here / The Way I Loved You / The Other Side of the Door_24-05-2024":
+    "https://www.tiktok.com/@swiftiesupremacy.13/video/7372796976561147141?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Message in a Bottle / How You Get The Girl / New Romantics_19-05-2024":
+    "https://www.tiktok.com/@kineaks/video/7370823403768532257?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Say Don't Go / Welcome to New York / Clean_18-05-2024":
+    "https://www.tiktok.com/@tiktokthepark/video/7371008336491203883?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "I Think He Knows / Gorgeous_17-05-2024":
+    "https://www.tiktok.com/@vickiesascha/video/7370415884931796256?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "The Alchemy / Treacherous_12-05-2024":
+    "https://www.tiktok.com/@jaime_thomas/video/7368319619041479978?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Begin Again / Paris_12-05-2024":
+    "https://www.tiktok.com/@renmarx/video/7368233516556963114?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Tim McGraw / cowboy like me_09-03-2024":
+    "https://www.tiktok.com/@addieblesausage/video/7344389074427399426?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "mirrorball / epiphany_09-03-2024":
+    "https://www.tiktok.com/@feverdreamhigh_/video/7344410013991046432?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Sparks Fly / gold rush_08-03-2024":
+    "https://www.tiktok.com/@nme/video/7344019421331082529?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  'False God / "Slut!"_08-03-2024':
+    "https://www.tiktok.com/@swiftiesupremacy.13/video/7350315288035970310?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Death by a Thousand Cuts / Babe_07-03-2024":
+    "https://www.tiktok.com/@ling2395/video/7343673254718000389?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Fifteen / You're on Your Own, Kid_07-03-2024":
+    "https://www.tiktok.com/@thefluffypao/video/7343659089160834312?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Foolish One / Tell Me Why_04-03-2024":
+    "https://www.tiktok.com/@yshunbin/video/7342528240277327111?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "This Love / Call It What You Want_04-03-2024":
+    "https://www.tiktok.com/@nathanieljvc/video/7342510352556428551?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "long story short / The Story of Us_03-03-2024":
+    "https://www.tiktok.com/@sue_enn/video/7342456658548755714?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Clean / evermore_03-03-2024":
+    "https://www.tiktok.com/@sue_enn/video/7342487877613374721?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Mine / Starlight_02-03-2024":
+    "https://www.tiktok.com/@dansihay/video/7343636427764256007?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "I Don't Wanna Live Forever / Dress_02-03-2024":
+    "https://www.tiktok.com/@nme/video/7341952188194852129?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Would've, Could've, Should've / ivy_26-02-2024":
+    "https://www.tiktok.com/@_lachlangrant/video/7339908245655440661?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Forever & Always / Maroon_26-02-2024":
+    "https://www.tiktok.com/@holly_e_miller/video/7339889284335357185?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "is it over now? / I Wish You Would_25-02-2024":
+    "https://www.tiktok.com/@mishytales/video/7339857782096596232?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Haunted / exile_25-02-2024":
+    "https://www.tiktok.com/@colorblindrobert/video/7339508755207294251?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Should've Said No / You're Not Sorry_24-02-2024":
+    "https://www.tiktok.com/@colorblindrobert/video/7339152586123005227?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "New Year's Day / peace_24-02-2024":
+    "https://www.tiktok.com/@colorblindrobert/video/7339152954479365418?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "White Horse / coney island_23-02-2024":
+    "https://www.tiktok.com/@sabslyrics/video/7338863108439493893?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Come Back… Be Here / Daylight_18-02-2024":
+    "https://www.tiktok.com/@teepeesandtreats/video/7336923460872047880?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "getaway car / august / the other side of the door_17-02-2024":
+    "https://www.tiktok.com/@foreverkiah/video/7336577808925281543?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+  "Is It Over Now? / Out of the woods_11-11-2023":
+    "https://www.tiktok.com/@miguelo_gp/video/7325547613158345990?is_from_webapp=1&sender_device=pc&web_id=7367105245619619361",
+};
