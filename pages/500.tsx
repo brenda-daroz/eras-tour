788174c853dde8fdf0526b696ed6f664fe35dab8
Brenda Daroz <brenda.daroz@gmail.com>
2024-07-28 11:45:38 +0200

    update 500 page

--- pages/500.tsx
+++ pages/500.tsx
@@ -2,9 +2,9 @@ import React from "react"
 export default function Custom500() {
   return (
     <div style={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-      <p style={{ color: "purple", fontSize: "1.1rem" }}>Oooops, something went wrong.</p>
+      <p style={{ color: "purple", fontSize: "1.1rem" }}>Oops, something went wrong. It's not you, it's us.</p>
       <iframe src="https://giphy.com/embed/WQCv56djMTCmQkySAg" style={{ border: 0 }} width="378" height="480" className="giphy-embed" allowFullScreen></iframe>
-      <h1 style={{ color: "purple" }}>Please, refresh the page.</h1>
+      <h1 style={{ color: "purple" }}>We're doing everything we can to bring the app back up and running.</h1>
     </div>
   )
 }
