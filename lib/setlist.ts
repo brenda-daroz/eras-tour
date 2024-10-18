b92aa76a8dad454da725e7e99cdb880cc020630c
Brenda Daroz <brenda.daroz@gmail.com>
2024-10-18 23:17:13 +0200

    update refresh based on time window

--- lib/setlist.ts
+++ lib/setlist.ts
@@ -6,8 +6,8 @@ import {
   discographySchema,
   setlistResponseSchema,
 } from "@/lib/logic";
-import fs from "fs";
-import setlistData from "../data/setlistData.js";
+// import fs from "fs";
+// import setlistData from "../data/setlistData.js";
 
 let cache: SetlistResponse | null = null;
 const SETLIST_API_KEY = process.env.SETLIST_API_KEY;
@@ -49,7 +49,7 @@ async function fetchSetlist(pageNumber: number) {
 
 export async function fetchAndTransformData(year?: number) {
   // for deployment:
-  // const setlistData = await readFromCache();
+  const setlistData = await readFromCache();
   // for development:
   // fs.writeFileSync("setlist.json", JSON.stringify(setlistData));
   // const setlistDataRaw = fs.readFileSync("data/setlistData.js", "utf8");
@@ -100,6 +100,21 @@ const readFromCache = async () => {
 };
 
 setInterval(() => {
-  console.info("Timebased refreshing cache");
-  fillCache().catch((error) => console.error("Error refreshing cache", error));
-}, 1000 * 60 * 11);
+  const now = new Date().getUTCHours();
+
+  const startHour = 0;
+  const endHour = 4;
+
+  if (now >= startHour && now < endHour) {
+    console.info("Time-based refreshing cache (UTC)");
+    fillCache().catch((error) => console.error("Error refreshing cache", error));
+  } else {
+    console.info("Cache refresh skipped. Outside of time window.");
+  }
+}, 1000 * 60 * 1);
+
+
+// setInterval(() => {
+//   console.info("Timebased refreshing cache");
+//   fillCache().catch((error) => console.error("Error refreshing cache", error));
+// }, 1000 * 60 * 5);
